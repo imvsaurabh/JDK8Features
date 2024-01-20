8a4e34a1a069d5cb3d9b45715d20f0b5899b429f
imvsaurabh <saurabh00v@gmail.com>
2024-01-20 20:20:40 +0530

    Added few more stream methods.

--- src/main/java/com/imvsaurabh/stream/JavaStream.java
+++ src/main/java/com/imvsaurabh/stream/JavaStream.java
@@ -1,5 +1,8 @@
 package com.imvsaurabh.stream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.Serializable;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -16,10 +19,12 @@ import static java.util.stream.Collectors.toSet;
 
 public class JavaStream {
 
+    private static final Logger log = LoggerFactory.getLogger(JavaStream.class);
+
     public static void main(String[] args) {
         Random random = new Random();
         List<Integer> randomNums = random.ints(20, 1, 20).boxed().toList();
-        System.out.println("Original List : " + randomNums);
+        log.info("Original List : {}", randomNums);
 
         List<? extends Serializable> objLst = Arrays.asList(5, "RAM", 8.0, "null", "Ravi", null);
 
@@ -29,34 +34,59 @@ public class JavaStream {
                 .entrySet().stream()
                 .filter(entry -> entry.getValue() > 1)
                 .map(Map.Entry::getKey).toList();
-        System.out.println("Duplicate Number List : " + duplicateNums1);
+        log.info("Duplicate Number List : {}", duplicateNums1);
 
 //        Find duplicate numbers from the list [Sol 2]
         Set<Integer> duplicateNums2 = randomNums.stream()
                 .filter(num -> frequency(randomNums, num) > 1)
                 .collect(toSet());
-        System.out.println("Duplicate Number List : " + duplicateNums2);
+        log.info("Duplicate Number List : {}", duplicateNums2);
 
 //        Find unique numbers using stream
         List<Integer> uniqueNums = randomNums.stream().distinct().toList();
-        System.out.println("Unique Number List : " + uniqueNums);
+        log.info("Unique Number List : {}", uniqueNums);
 
 //        Find even unique number
         List<Integer> evenNums = randomNums.stream()
                 .filter(num -> num % 2 == 0)
                 .distinct().toList();
-        System.out.println("Even Number List : " + evenNums);
+        log.info("Even Number List : {}", evenNums);
 
 //        Check if any number in between 5 & 9
         boolean isNumFoundBetween5and9 = randomNums.stream()
                 .anyMatch(num -> num > 5 && num < 9);
-        System.out.println("isNumFoundBetween5and9 : " + isNumFoundBetween5and9);
+        log.info("isNumFoundBetween5and9 : {}", isNumFoundBetween5and9);
 
 //        Check if objLst contains any null value
         boolean isContainsNull = objLst.stream()
                 .anyMatch(Objects::isNull);
-        System.out.println("objLst isContainsNull : " + isContainsNull);
+        log.info("objLst isContainsNull : {}", isContainsNull);
+
+//        Maximum number
+        Integer maxNum = randomNums.stream().max(Comparator.naturalOrder()).orElse(0);
+        log.info("Maximum number from the randomNums : {}", maxNum);
+
+//        Minimum number
+        Integer minNum = randomNums.stream().min(Comparator.naturalOrder()).orElse(0);
+        log.info("Minimum number from the randomNums : {}", minNum);
+
+//        Average number
+        Integer sumNum = randomNums.stream().reduce(Integer::sum).orElse(0);
+        log.info("Average number from the randomNums : {}", sumNum / randomNums.size());
+
+//        Sum of all digit of a number
+        int digitSum = Arrays.stream(String.valueOf(sumNum).split(""))
+                .mapToInt(Integer::parseInt).sum();
+        log.info("Sum of digits of number {} is {}", sumNum, digitSum);
+
+//        Second smallest number
+        Integer secondSmallestNumber = randomNums.stream().distinct().sorted().skip(1).findFirst().orElse(0);
+        log.info("2nd smallest number from the randomNums : {}", secondSmallestNumber);
+
+//        Second largest number
+        Integer secondLargestNumber = randomNums.stream().distinct().sorted(Comparator.reverseOrder()).skip(1).findFirst().orElse(0);
+        log.info("2nd largest number from the randomNums : {}", secondLargestNumber);
+
 
     }
-
 }
